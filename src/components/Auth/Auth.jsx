2de91cd642e1ee6033c3ce280f337878983704be
forestheims <forestheims@gmail.com>
2022-04-16 11:12:35 -0700

    added authForm component, using the useForm hook to manage form state, and styled slightly

--- src/components/Auth/Auth.jsx
+++ src/components/Auth/Auth.jsx
@@ -1,11 +1,49 @@
 import React from 'react';
+import { useForm } from '../../hooks/useForm';
+import styles from './Auth.css';
 
 export default function Auth({ isRegistering = false }) {
   // BACKEND CONNECTION
   // if isRegistering
-  // POST to users and profiles
+
   // else
   // POST/PATCH??
+  const { formState, formError, handleFormChange, setFormError } = useForm();
+
   console.log('isRegistering', isRegistering);
-  return <div>{isRegistering ? 'registering' : 'logging in'}</div>;
+
+  const handleAuthSubmit = () => {
+    if (isRegistering) {
+      // Create account (user and profile)
+      // POST to users and profiles
+    } else {
+      // Log In
+    }
+  };
+
+  return (
+    <form className={styles.authForm} onSubmit={handleAuthSubmit}>
+      <h2>{isRegistering ? 'Register' : 'Log In'}</h2>
+      <label>
+        Username:
+        <input
+          type="text"
+          name="username"
+          value={formState.username}
+          onChange={(e) => handleFormChange(e)}
+        />
+      </label>
+      {/* maybe refactor to a file upload system? */}
+      <label>
+        Password:
+        <input
+          type="text"
+          name="password"
+          value={formState.password}
+          onChange={(e) => handleFormChange(e)}
+        />
+      </label>
+      <button>{isRegistering ? 'Register' : 'Log In'}</button>
+    </form>
+  );
 }
