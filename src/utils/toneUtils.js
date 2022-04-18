87272d8c661338cedacab2122ba45a1840beb9cb
Michelle Nygren <michelle.r.nygren@gmail.com>
2022-04-18 11:00:35 -0700

    Set global data for sequencer in toneUtils file

---
+++ src/utils/toneUtils.js
@@ -0,0 +1,9 @@
+// global data for sequencer
+
+const keyCMajorPentatonic = ['C3', 'D3', 'E3', 'G3', 'A3', 'C4'];
+
+const oscillators = ['triangle', 'sine', 'square'];
+
+const instruments = ['', 'duoSynth', 'membraneSynth', 'monoSynth', 'synth'];
+
+export { keyCMajorPentatonic, oscillators, instruments };
