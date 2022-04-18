02f12b8be411118eafa1532f773c1237c19feac9
Michelle Nygren <michelle.r.nygren@gmail.com>
2022-04-18 11:02:36 -0700

    Update toneUtils fine with setPitchColor functionality

--- src/utils/toneUtils.js
+++ src/utils/toneUtils.js
@@ -1,4 +1,5 @@
 // global data for sequencer
+// import styles from '../components/Channel/Channel.css';
 
 const keyCMajorPentatonic = ['C3', 'D3', 'E3', 'G3', 'A3', 'C4'];
 
@@ -6,4 +7,14 @@ const oscillators = ['triangle', 'sine', 'square'];
 
 const instruments = ['', 'duoSynth', 'membraneSynth', 'monoSynth', 'synth'];
 
-export { keyCMajorPentatonic, oscillators, instruments };
+const setPitchColor = (string) => {
+  return {
+    [styles.C]: string?.includes('C'),
+    [styles.D]: string?.includes('D'),
+    [styles.E]: string?.includes('E'),
+    [styles.G]: string?.includes('G'),
+    [styles.A]: string?.includes('A'),
+  };
+};
+
+export { keyCMajorPentatonic, oscillators, instruments, setPitchColor };
