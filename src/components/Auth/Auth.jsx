144e2b34b7037823b60b19de2e8354bbf63d9cd5
forestheims <forestheims@gmail.com>
2022-04-18 20:02:01 -0700

    MemoryRouter added to tests. updated packages for @testing-library/react and jest.

--- src/components/Auth/Auth.jsx
+++ src/components/Auth/Auth.jsx
@@ -2,13 +2,13 @@ import React from 'react';
 import { useForm } from '../../hooks/useForm';
 import styles from './Auth.css';
 import { registerUser, signInUser } from '../../services/users';
-import { useNavigate } from 'react-router-dom';
+// import { useNavigate } from 'react-router-dom';
 
 export default function Auth({ isRegistering = false }) {
   const { formState, formMessage, handleFormChange, setFormMessage } =
     useForm();
 
-  const navigate = useNavigate();
+  // const navigate = useNavigate();
 
   const handleAuthSubmit = async (e) => {
     e.preventDefault();
@@ -16,7 +16,7 @@ export default function Auth({ isRegistering = false }) {
       const user = await registerUser(formState.username, formState.password);
       if (user?.username) {
         setFormMessage('you are registered');
-        navigate(`/signin`, { push: true });
+        // navigate(`/signin`, { push: true });
       }
     } else {
       const { message } = await signInUser(
@@ -24,7 +24,7 @@ export default function Auth({ isRegistering = false }) {
         formState.password
       );
       setFormMessage(message);
-      navigate(`/user/${formState.username}`, { push: true });
+      // navigate(`/user/${formState.username}`, { push: true });
     }
   };
 
--- src/tests/auth.test.js
+++ src/tests/auth.test.js
@@ -1,14 +1,24 @@
 import { render, screen } from '@testing-library/react';
-import Auth from './components/Auth/Auth';
+import { MemoryRouter } from 'react-router-dom';
+import Auth from '../components/Auth/Auth';
+import { UserContext } from '../context/UserContext';
 
 test('renders Auth form component in a registering state', () => {
-  render(<Auth isRegistering />);
+  render(
+    <MemoryRouter>
+      <Auth isRegistering />
+    </MemoryRouter>
+  );
   const formHeader = screen.getByRole('heading');
   expect(formHeader.innerHTML).toEqual('Register');
 });
 
 test('renders Auth component in a Logging In state', () => {
-  render(<Auth />);
+  render(
+    <MemoryRouter>
+      <Auth />
+    </MemoryRouter>
+  );
   const formHeader = screen.getByRole('heading');
   expect(formHeader.innerHTML).toEqual('Log In');
 });
