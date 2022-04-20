520f0a43889554f60a29fa87ef26d960ddebaaad
minoka kakizaki <minoka.kakizaki@gmail.com>
2022-04-19 18:53:20 -0700

    the save project button save and redirects to the profile page

--- src/views/Profile/Profile.jsx
+++ src/views/Profile/Profile.jsx
@@ -46,12 +46,12 @@ export default function Profile() {
       try {
         const data = await findProfileByUsername(username);
         setUserProfile(data);
-        const projects = await findProjectsByUserId(data.userId);
-        setProjects(projects);
-        console.log('projects', projects); // TODO: Check data model for authentication params
+        // const projects = await findProjectsByUserId(data.userId);
+        // setProjects(projects);
+        // console.log('projects', projects); // TODO: Check data model for authentication params
       } catch (error) {
         setUserProfile({}); // TODO: Do we need this fallback?
-        setProjects([]);
+        // setProjects([]);
       }
       setLoading(false);
     };
