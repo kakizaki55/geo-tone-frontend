a8430670beb49e6c032cc06a28c82a8e1a273ec3
forestheims <forestheims@gmail.com>
2022-04-20 07:05:54 -0700

    informs a user if a username is already taken. Informs user if the username contains any non-allowed characters.

--- src/components/Auth/Auth.jsx
+++ src/components/Auth/Auth.jsx
@@ -6,18 +6,30 @@ import { useUser } from '../../context/UserContext';
 import { useNavigate } from 'react-router-dom';
 
 export default function Auth({ isRegistering = false }) {
-  const { formState, formMessage, handleFormChange, setFormMessage } =
-    useForm();
+  const { formState, formMessage, handleFormChange, setFormMessage } = useForm({
+    username: '',
+    password: '',
+  });
   const navigate = useNavigate();
   const { currentUser, setCurrentUser } = useUser();
+
+  const onlyLettersAndNumbers = (str) => {
+    return /[A-Za-z0-9]g/.test(str);
+  };
 
   const handleAuthSubmit = async (e) => {
     e.preventDefault();
     if (isRegistering) {
-      const user = await registerUser(formState.username, formState.password);
-      if (user?.username) {
+      if (!onlyLettersAndNumbers(formState.username)) {
+        setFormMessage('usernames can only contain lettes and numbers');
+      } else if (user?.username) {
+        const user = await registerUser(formState.username, formState.password);
         setFormMessage('you are registered');
         navigate(`/signin`, { push: true });
+      } else if (user.message === 'username already exists') {
+        setFormMessage(
+          'that username is already taken. please choose a different username.'
+        );
       }
     } else {
       const { message } = await signInUser(
@@ -47,7 +59,7 @@ export default function Auth({ isRegistering = false }) {
       <label>
         Password:
         <input
-          type="text"
+          type="password"
           name="password"
           value={formState.password}
           onChange={(e) => handleFormChange(e)}
