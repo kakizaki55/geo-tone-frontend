174d12bde7b4f2066e972871a87793166bf6b5a4
jlaurentpdx <jlaurent.pdx@gmail.com>
2023-07-28 17:25:47 -0700

    removes user content from home

--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -1,17 +1,7 @@
-import { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
-// import { getTotalUsers, getTotalProjects } from '../../services/aggregate';
 import shapes from '../../../public/assets/shapes.png';
 import styles from './Home.css';
 
-
 export default function Home() {
-  const [totalUsers, setTotalUsers] = useState(0);
-  const [totalProjects, setTotalProjects] = useState(0);
-
-  useEffect(() => {
-  }, []);
-
   return (
     <>
       <section className={styles.homeContainer}>
@@ -21,17 +11,6 @@ export default function Home() {
           <img src={shapes} alt="Colorful squares" />
         </div>
         <p>A browser-based audio sequencer, built with creativity in mind.</p>
-        <p>
-          Love music? Us, too!{' '}
-          <Link to="/register" className={styles.registerLink}>
-            Sign up
-          </Link>{' '}
-          to play.
-        </p>
-        <p>
-          Join the {totalUsers} other synth wizards who have created{' '}
-          {totalProjects} projects!
-        </p>
       </section>
     </>
   );
