2747c4613af7200a99bd62eab06f63232371b4ee
jlaurentpdx <jlaurent.pdx@gmail.com>
2023-07-30 19:18:56 -0700

    adds "create" button to home page, minor style fix

--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -1,17 +1,19 @@
+import { NavLink } from 'react-router-dom';
 import shapes from '../../../public/assets/shapes.png';
 import styles from './Home.css';
 
 export default function Home() {
   return (
-    <>
-      <section className={styles.homeContainer}>
-        <div className={styles.homeBanner}>
-          <img src={shapes} alt="Colorful squares" />
-          <h1>Welcome to GEo TONe.</h1>
-          <img src={shapes} alt="Colorful squares" />
-        </div>
-        <p>A browser-based audio sequencer, built with creativity in mind.</p>
-      </section>
-    </>
+    <section>
+      <div className={styles.homeBanner}>
+        <img src={shapes} alt="" />
+        <h1>Welcome to GEo TONe.</h1>
+        <img src={shapes} alt="" />
+      </div>
+      <p>A browser-based audio sequencer, built with creativity in mind.</p>
+      <NavLink to="/create" className={styles.navButton}>
+        <button>crEaTE</button>
+      </NavLink>
+    </section>
   );
 }
