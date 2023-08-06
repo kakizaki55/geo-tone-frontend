d89dd78b8f16679468372a4754e814042d38b65a
jlaurentpdx <jlaurent.pdx@gmail.com>
2023-08-05 17:34:45 -0700

    adds id property to VolumeFader

--- src/components/Controls/VolumeFader/VolumeFader.jsx
+++ src/components/Controls/VolumeFader/VolumeFader.jsx
@@ -1,10 +1,16 @@
 import styles from './VolumeFader.css';
 
-export default function VolumeFader({ isGlobal = false, volume, setVolume }) {
+export default function VolumeFader({
+  id,
+  volume,
+  setVolume,
+  isGlobal = false,
+}) {
   return (
     <label className={styles.label}>
       {isGlobal ? 'Project Volume' : 'Volume'}
       <input
+        id={isGlobal ? 'global-volume' : id}
         type="range"
         min="-48"
         max="0"
