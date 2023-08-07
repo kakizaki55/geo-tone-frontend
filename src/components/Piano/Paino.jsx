979a84554493d3d5212a1d4460dc74eca319b1a0
minoka kakizaki <minoka.kakizaki@gmail.com>
2023-08-07 14:41:00 -0700

    hot fix for audio popping

--- src/components/Piano/Paino.jsx
+++ src/components/Piano/Paino.jsx
@@ -9,7 +9,7 @@ import EffectsRack from "./EffectsRack"
 
 
 const Piano = () => {
-  const [volume, setVolume] = useState(-3);
+  const [volume, setVolume] = useState(-40);
   const [fx, setFx] = useState({
     autoFilter:0,
     autoPanner:0,
