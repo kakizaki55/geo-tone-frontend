ea6e72c2f549a95305e3e971967ef127d79d7a43
jlaurentpdx <jlaurent.pdx@gmail.com>
2023-08-10 22:12:27 -0700

    updates GlobalControls with new components

--- src/components/editor/controls/GlobalControls/GlobalControls.jsx
+++ src/components/editor/controls/GlobalControls/GlobalControls.jsx
@@ -1,51 +1,20 @@
-import { motion, useCycle } from 'framer-motion';
 import { useProject } from '@context/ProjectContext';
-import { playVariants as play } from '@utils/framer-constants';
+import { BPMCounter, PlayButton, VolumeFader } from '../index.js';
 import styles from './GlobalControls.css';
 
 export default function GlobalControls({ start, setStart, volume, setVolume }) {
   const { project, handleSongBPM } = useProject();
 
-  const [active, cycleActive] = useCycle('play', 'stop');
-
   return (
     <div id="global-controls" className={styles.container}>
-      <motion.button
-        className={styles.playButton}
-        whileHover={{ scale: 1.1 }}
-        onClick={() => {
-          cycleActive();
-          setStart(!start);
-        }}
-        animate={active}
-        variants={play}
-      >
-        {start ? 'stop' : 'play'}
-      </motion.button>
-      <label className={styles.label}>
-        Project Volume
-        <input
-          type="range"
-          min="-48"
-          max="0"
-          step="1"
-          value={volume}
-          onChange={(e) => setVolume(Number(e.target.value))}
-        />
-      </label>
-
-      <label className={styles.label}>
-        BPM
-        <input
-          type="number"
-          min="0"
-          max="440"
-          step="10"
-          value={project.bpm}
-          onChange={handleSongBPM}
-          className={styles.bpmCounter}
-        />
-      </label>
+      <PlayButton start={start} setStart={setStart} />
+      <VolumeFader
+        isGlobal
+        id={'global'}
+        value={volume}
+        handleChange={(e) => setVolume(e.target.value)}
+      />
+      <BPMCounter project={project} handleSongBPM={handleSongBPM} />
     </div>
   );
 }
--- src/components/editor/controls/index.js
+++ src/components/editor/controls/index.js
@@ -1,8 +1,19 @@
 // handles exports of all components in current folder
 
+import BPMCounter from './BPMCounter/BPMCounter.jsx';
 import Channel from './Channel/Channel.jsx';
 import Controls from './TrackControls/TrackControls.jsx';
 import Dropdown from './Dropdown/Dropdown.jsx';
+import PlayButton from './PlayButton/PlayButton.jsx';
+import VolumeFader from './VolumeFader/VolumeFader.jsx';
 import GlobalControls from './GlobalControls/GlobalControls.jsx';
 
-export { Channel, Controls, Dropdown, GlobalControls };
+export {
+  BPMCounter,
+  Channel,
+  Controls,
+  Dropdown,
+  PlayButton,
+  VolumeFader,
+  GlobalControls,
+};
