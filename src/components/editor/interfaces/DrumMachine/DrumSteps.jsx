054d3982450f6337ee53c00b7cc491f2cec98007
jlaurentpdx <jlaurent.pdx@gmail.com>
2023-08-10 21:19:29 -0700

    fixes React key warning for array.map

--- src/components/editor/interfaces/DrumMachine/DrumSteps.jsx
+++ src/components/editor/interfaces/DrumMachine/DrumSteps.jsx
@@ -1,43 +1,42 @@
-import React from 'react'
-import styles from './DrumMachine.css'
-import Pad from "./Pad"
-import DrumDial from "./DrumDial"
+import React from 'react';
+import styles from './DrumMachine.css';
+import Pad from './Pad';
+import DrumDial from './DrumDial';
 
 const DrumSteps = (props) => {
-
-  const {
-    drumProps
-  } = props
+  const { drumProps } = props;
 
   return (
     <>
-      { drumProps.map((drum)=> {
-          return (
-            <div className={styles.drumStepsContainer}>
-              <div>
-                { drum.steps.map((midi, index) => {
-                  return (
-                    <Pad
-                      key={`step-${index}`}
-                      note={midi}
-                      index={index}
-                      handleDrumChange={drum.handleDrumChange}
-                      drums={drum.steps}
-                      setDrums={drum.setSteps}
-                    />
-                    )})}
-              </div>
-              <div className={styles.volume}>
-                Volume
-                <DrumDial
-                  value={drum.volume}
-                  setValue={drum.setVolume}>
-                </DrumDial>
-              </div>
+      {drumProps.map((drum) => {
+        return (
+          <div key={drum.type} className={styles.drumStepsContainer}>
+            <div>
+              {drum.steps.map((midi, index) => {
+                return (
+                  <Pad
+                    key={`step-${index}`}
+                    note={midi}
+                    index={index}
+                    handleDrumChange={drum.handleDrumChange}
+                    drums={drum.steps}
+                    setDrums={drum.setSteps}
+                  />
+                );
+              })}
             </div>
-      )})}
+            <div className={styles.volume}>
+              Volume
+              <DrumDial
+                value={drum.volume}
+                setValue={drum.setVolume}
+              ></DrumDial>
+            </div>
+          </div>
+        );
+      })}
     </>
-  )
-}
+  );
+};
 
-export default DrumSteps
+export default DrumSteps;
