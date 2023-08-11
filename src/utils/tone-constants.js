8f653f9e8986b3e252841786467bf72f0507802c
jlaurentpdx <jlaurent.pdx@gmail.com>
2023-08-10 18:43:52 -0700

    adds constants for Tone/Reactronica params, prepares some exports for deprecation

--- src/utils/tone-constants.js
+++ src/utils/tone-constants.js
@@ -1,30 +1,80 @@
 // global data for sequencer
 import styles from '@components/editor/controls/Channel/Channel.css';
 
-// tonejs constants
+const globalParams = {
+  volume: {
+    default: '-12',
+    min: -60, // no standard decibel minimum could be identified; -60 dB is arbitrary (JL)
+    max: 0,
+    step: 1,
+  },
+  fx: {
+    default: 0.0,
+    min: 0.0,
+    max: 1.0,
+    step: 0.05,
+  },
+  bpm: {
+    default: 120,
+    min: 60,
+    max: 360,
+    step: 4,
+  },
+};
 
-const keyCMajorPentatonic4 = ['C4', 'D4', 'E4', 'G4', 'A4', 'C5'];
-const keyCMajorPentatonic3 = ['C3', 'D3', 'E3', 'G3', 'A3', 'C4'];
-const keyCMajorPentatonic2 = ['C2', 'D2', 'E2', 'G2', 'A2', 'C3'];
+const keys = {
+  CMajorPentatonic2: ['C2', 'D2', 'E2', 'G2', 'A2', 'C3'],
+  CMajorPentatonic3: ['C3', 'D3', 'E3', 'G3', 'A3', 'C4'],
+  CMajorPentatonic4: ['C4', 'D4', 'E4', 'G4', 'A4', 'C5'],
+  pianoChromatic: [
+    'C3',
+    'C#3',
+    'D3',
+    'D#3',
+    'E3',
+    'F3',
+    'F#3',
+    'G3',
+    'G#3',
+    'A3',
+    'A#3',
+    'B3',
+    'C4',
+    'C#4',
+    'D4',
+    'D#4',
+    'E4',
+    'F4',
+  ],
+};
 
 const oscillators = ['triangle', 'sine', 'square'];
 
-const sequencerInstrumentsTypes = [
-  'choose your sound',
-  'duoSynth',
-  'monoSynth',
-  'membraneSynth',
+const synthTypes = {
+  sequencer: ['duoSynth', 'monoSynth', 'membraneSynth'],
+  piano: [
+    'amSynth',
+    'duoSynth',
+    'fmSynth',
+    'membraneSynth',
+    'monoSynth',
+    'pluckSynth',
+  ],
+};
+
+const fxTypes = [
+  'autoFilter',
+  'autoPanner',
+  'autoWah',
+  'bitCrusher',
+  'distortion',
+  'feedbackDelay',
+  'freeverb',
+  'panVol',
+  'tremolo',
 ];
 
-const pianoInstrumentType = [
-  'amSynth',
-  'duoSynth',
-  'fmSynth',
-  'membraneSynth',
-  'monoSynth',
-  'pluckSynth',
-];
-
+// ! Deprecated - use fxTypes instead
 const pianoEffectTypes = [
   'autoFilter',
   'autoPanner',
@@ -37,6 +87,30 @@ const pianoEffectTypes = [
   'tremolo',
 ];
 
+// ! Deprecated - use keys.CMajorPentatonic[#] instead
+const keyCMajorPentatonic4 = ['C4', 'D4', 'E4', 'G4', 'A4', 'C5'];
+const keyCMajorPentatonic3 = ['C3', 'D3', 'E3', 'G3', 'A3', 'C4'];
+const keyCMajorPentatonic2 = ['C2', 'D2', 'E2', 'G2', 'A2', 'C3'];
+
+// ! Deprecated - use synthTypes.sequencer instead
+const sequencerInstrumentsTypes = [
+  'choose your sound',
+  'duoSynth',
+  'monoSynth',
+  'membraneSynth',
+];
+
+// ! Deprecated - use synthTypes.piano instead
+const pianoInstrumentType = [
+  'amSynth',
+  'duoSynth',
+  'fmSynth',
+  'membraneSynth',
+  'monoSynth',
+  'pluckSynth',
+];
+
+// ! Deprecated - use keys.pianoChromatic instead
 const pianoChromaticScale = [
   'C3',
   'C#3',
@@ -71,13 +145,18 @@ const setPitchColor = (string) => {
 };
 
 export {
-  keyCMajorPentatonic2,
-  keyCMajorPentatonic3,
-  keyCMajorPentatonic4,
+  globalParams,
+  keys,
   oscillators,
-  sequencerInstrumentsTypes,
-  pianoInstrumentType,
-  pianoChromaticScale,
-  pianoEffectTypes,
+  synthTypes,
+  fxTypes,
   setPitchColor,
+  // ! Items below are deprecated - check above for alternatives
+  pianoEffectTypes,
+  keyCMajorPentatonic4,
+  keyCMajorPentatonic3,
+  keyCMajorPentatonic2,
+  pianoChromaticScale,
+  pianoInstrumentType,
+  sequencerInstrumentsTypes,
 };
