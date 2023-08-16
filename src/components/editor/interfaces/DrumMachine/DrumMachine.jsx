93c61bd7b477a7913b5075d1723ff846921399d2
jlaurentpdx <jlaurent.pdx@gmail.com>
2023-08-16 15:26:18 -0700

    updates DrumMachine with new control components, removes deprecated

--- src/components/editor/interfaces/DrumMachine/DrumMachine.jsx
+++ src/components/editor/interfaces/DrumMachine/DrumMachine.jsx
@@ -1,96 +1,47 @@
-import { useEffect, useState } from 'react';
-import { Track, Instrument } from 'reactronica';
+import { useReducer } from 'react';
 import { useProject } from '@context/ProjectContext';
-import { handleDrumChange, highlightCurrentStep } from '@utils/interface-utils';
-import DrumSteps from './DrumSteps';
+import { DrumChannel } from '@components/editor/controls';
 import styles from './DrumMachine.css';
 
 const DrumMachine = () => {
-  const { project, handleUpdateDrums } = useProject();
+  const { project } = useProject();
 
-  const [highHat, setHighHat] = useState(project.drums[0].steps);
-  const [highHatVolume, setHighHatVolume] = useState(project.drums[0].volume);
-  const [snare, setSnare] = useState(project.drums[1].steps);
-  const [snareVolume, setSnareVolume] = useState(project.drums[1].volume);
-  const [kick, setKick] = useState(project.drums[2].steps);
-  const [kickVolume, setKickVolume] = useState(project.drums[2].volume);
+  const [drums, dispatch] = useReducer(drumsReducer, project.drums);
 
-  //this use Effect makes a new drum object and send its back up in the Project Context
-  useEffect(() => {
-    const drumObj = [
-      {
-        type: 'high-hat',
-        steps: highHat,
-        volume: highHatVolume,
-      },
-      {
-        type: 'snare',
-        steps: snare,
-        volume: snareVolume,
-      },
-      {
-        type: 'kick',
-        steps: kick,
-        volume: kickVolume,
-      },
-    ];
-
-    handleUpdateDrums(drumObj);
-  }, [highHat, snare, kick, highHatVolume, snareVolume, kickVolume]);
-
-  const drumProps = [
-    {
-      type: 'hightHat',
-      steps: highHat,
-      setSteps: setHighHat,
-      volume: highHatVolume,
-      setVolume: setHighHatVolume,
-      handleDrumChange: handleDrumChange,
-    },
-    {
-      type: 'snare',
-      steps: snare,
-      setSteps: setSnare,
-      volume: snareVolume,
-      setVolume: setSnareVolume,
-      handleDrumChange: handleDrumChange,
-    },
-    {
-      type: 'kick',
-      steps: kick,
-      setSteps: setKick,
-      volume: kickVolume,
-      setVolume: setKickVolume,
-      handleDrumChange: handleDrumChange,
-    },
-  ];
+  const handleUpdateDrum = (drum) => {
+    dispatch({
+      type: 'update a drum channel',
+      task: drum,
+    });
+  };
 
   return (
     <div className={styles.drumMachineContainer}>
-      {project.drums.map((value) => {
-        return (
-          <Track
-            steps={value.steps}
-            key={value.type}
-            onStepPlay={(step, stepIndex) =>
-              highlightCurrentStep('drumPadOn', stepIndex, styles)
-            }
-            volume={value.volume}
-          >
-            <Instrument
-              type="sampler"
-              samples={{ C3: `/assets/samples/${value.type}.mp3` }}
-              onLoad={(buffers) => {
-                // Runs when all samples are loaded
-              }}
-            />
-          </Track>
-        );
-      })}
-      {/* Render all visual components below*/}
-      <DrumSteps drumProps={drumProps} />
+      {drums.map((drum) => (
+        <DrumChannel
+          key={`drum-${drum.type}`}
+          drum={drum}
+          handleUpdate={handleUpdateDrum}
+        />
+      ))}
     </div>
   );
 };
+
+function drumsReducer(drums, action) {
+  switch (action.type) {
+    case 'update a drum channel': {
+      return drums.map((channel) => {
+        if (channel.type === action.task.type) {
+          return action.task;
+        } else {
+          return channel;
+        }
+      });
+    }
+    default:
+      throw new Error(`Unknown action ${action.type}`);
+  }
+}
 
 export default DrumMachine;
