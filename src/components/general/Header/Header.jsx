eb987f7792b4b3871e57c6a9ea9422153b5ff6ef
jlaurentpdx <jlaurent.pdx@gmail.com>
2023-08-21 16:05:17 -0700

    moves header styling to component-level

--- src/components/general/Header/Header.jsx
+++ src/components/general/Header/Header.jsx
@@ -1,16 +1,18 @@
 import { NavLink } from 'react-router-dom';
-import styles from '../Layout/Layout.css';
+import styles from './Header.css';
 
 const Header = () => {
   return (
     <header>
-      <NavLink className={styles.logo} to="/">
-        GEo TONe
-      </NavLink>
-      <div>
-        <NavLink to="/create">Create</NavLink>
-        <NavLink to="/about">About</NavLink>
-      </div>
+      <nav className={styles.navigation}>
+        <NavLink className={styles.logo} to="/">
+          GEo TONe
+        </NavLink>
+        <div>
+          <NavLink to="/create">Create</NavLink>
+          <NavLink to="/about">About</NavLink>
+        </div>
+      </nav>
     </header>
   );
 };
